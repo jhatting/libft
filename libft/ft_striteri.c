4b812ac3718c8219f0106a01a210524e4775676e
Scrumpy Hat <shat@e1z3r2p2.42.us.org>
2019-09-10 13:01:46 -0700

    update

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,11 +6,17 @@
 /*   By: shat <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 22:28:41 by shat              #+#    #+#             */
-/*   Updated: 2019/08/28 22:28:54 by shat             ###   ########.fr       */
+/*   Updated: 2019/09/10 13:00:43 by shat             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Applies the function f to each character of the string passed as argument,
+** and passing its index as first argument. Each character is passed by
+** address to f to be modified if necessary.
+*/
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
