0f6e0790fe9d74befdc75561d784f558de2dc4b6
Scrumpy Hat <shat@e1z2r7p10.42.us.org>
2019-09-11 16:44:10 -0700

    update

--- libft/ft_memalloc.c
+++ libft/ft_memalloc.c
@@ -6,11 +6,17 @@
 /*   By: shat <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 00:02:33 by shat              #+#    #+#             */
-/*   Updated: 2019/09/05 21:46:24 by shat             ###   ########.fr       */
+/*   Updated: 2019/09/11 14:38:25 by shat             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**Allocates with malloc(3) and returns a fresh memory area.
+**The memory allocated is initialized to 0. If the allocation
+**fails, the function returns NULL.
+*/
 
 void	*ft_memalloc(size_t size)
 {
