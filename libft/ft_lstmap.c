c6a86120e6e860809d7acf340b095731cebd0c5c
Scrumpy Hat <shat@e1z2r7p10.42.us.org>
2019-09-12 16:11:20 -0700

    update sept 12th

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,11 +6,18 @@
 /*   By: shat <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 13:26:18 by shat              #+#    #+#             */
-/*   Updated: 2019/09/03 13:26:26 by shat             ###   ########.fr       */
+/*   Updated: 2019/09/12 16:09:07 by shat             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**The ft_map() function iterates a list lst and applies the function f
+**to each link to create a “fresh” list (using malloc(3)) resulting from
+**the successive applications of f. If the allocation fails, the function
+**returns NULL** 
+*/
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
